c13accec8cebb8b90db9a98466b716882eca96e2
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-28 09:44:13 -0700

    Add: Launchschool exercises on variables

---
+++ launchschool/name.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+name = gets.chomp
+puts "Greets to you, " + name
+
+10.times { |number| puts name }
+
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Greetings to you, #{first_name} #{last_name}." 
