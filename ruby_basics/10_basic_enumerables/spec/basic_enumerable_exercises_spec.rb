cf519744213be5b2b3bf93582bcfc4758761a96e
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-02-05 10:41:07 -0700

    Add: Basic enumerable exercises with rdbg help

--- ruby_basics/10_basic_enumerables/spec/basic_enumerable_exercises_spec.rb
+++ ruby_basics/10_basic_enumerables/spec/basic_enumerable_exercises_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 require_relative '../exercises/basic_enumerable_exercises'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
 
 RSpec.describe 'Basic Enumerable Exercises' do
 
@@ -14,7 +25,7 @@ RSpec.describe 'Basic Enumerable Exercises' do
     end
 
     # remove the 'x' from the line below to unskip the test
-    xit 'outputs item without quantity when value is nil' do
+    it 'outputs item without quantity when value is nil' do
       expect($stdout).to receive(:puts).with("pineapples, quantity: ")
       fruit = { pineapples: nil }
       display_current_inventory(fruit)
@@ -23,7 +34,7 @@ RSpec.describe 'Basic Enumerable Exercises' do
   
   describe 'display guess order exercise' do
     
-    xit 'outputs each guess of strings in order' do
+    it 'outputs each guess of strings in order' do
       expect($stdout).to receive(:puts).with("Guess #1 is cookies")
       expect($stdout).to receive(:puts).with("Guess #2 is cake")
       expect($stdout).to receive(:puts).with("Guess #3 is ice cream")
@@ -31,7 +42,7 @@ RSpec.describe 'Basic Enumerable Exercises' do
       display_guess_order(guesses)
     end
 
-    xit 'outputs each guess of integers in order' do
+    it 'outputs each guess of integers in order' do
       expect($stdout).to receive(:puts).with("Guess #1 is 553")
       expect($stdout).to receive(:puts).with("Guess #2 is 554")
       expect($stdout).to receive(:puts).with("Guess #3 is 555")
@@ -42,13 +53,13 @@ RSpec.describe 'Basic Enumerable Exercises' do
 
   describe 'find absolute values exercise' do
     
-    xit 'returns an array of positive integers' do
+    it 'returns an array of positive integers' do
       numbers = [0, -7, 14, -21]
       result = [0, 7, 14, 21]
       expect(find_absolute_values(numbers)).to eq(result)
     end
 
-    xit 'returns an array of positive floating point numbers' do
+    it 'returns an array of positive floating point numbers' do
       numbers = [-3.14, 6.28, -9.42]
       result = [3.14, 6.28, 9.42]
       expect(find_absolute_values(numbers)).to eq(result)
@@ -57,13 +68,13 @@ RSpec.describe 'Basic Enumerable Exercises' do
 
   describe 'find low inventory exercise' do
     
-    xit 'returns a hash with integer values' do
+    it 'returns a hash with integer values' do
       fruit = { apples: 1, peaches: 4, bananas: 3, oranges: 7 }
       result = { apples: 1, bananas: 3 }
       expect(find_low_inventory(fruit)).to eq(result)
     end
 
-    xit 'returns a hash with floating point number values' do
+    it 'returns a hash with floating point number values' do
       cakes = { chocolate_cake: 2.5, vanilla_cake: 4.25, carrot_cake: 3.75 }
       result = { chocolate_cake: 2.5, carrot_cake: 3.75 }
       expect(find_low_inventory(cakes)).to eq(result)
@@ -72,13 +83,13 @@ RSpec.describe 'Basic Enumerable Exercises' do
 
   describe 'find word length exercise' do
     
-    xit 'returns a hash with rocket syntax when using strings' do
+    it 'returns a hash with rocket syntax when using strings' do
       animals = ['cat', 'horse', 'rabbit', 'deer']
       result = { 'cat' => 3, 'horse' => 5, 'rabbit' => 6, 'deer' => 4 }
       expect(find_word_lengths(animals)).to eq(result)
     end
     
-    xit 'returns a hash with symbols syntax when using symbols' do
+    it 'returns a hash with symbols syntax when using symbols' do
       animals = [:cat, :horse, :rabbit, :deer]
       result = { cat: 3, horse: 5, rabbit: 6, deer: 4 }
       expect(find_word_lengths(animals)).to eq(result)
