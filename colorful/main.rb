7d07444168adc4d6d14797f443b51a0cc6f64afa
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-05-14 10:40:20 -0600

    feat: add colorize library practice

---
+++ colorful/main.rb
@@ -0,0 +1,16 @@
+require 'colorize'
+
+puts 'Red goes faster!'.colorize(:red)
+
+puts "I'm blue da ba dee da ba di!".colorize(:blue)
+
+puts "It ain't easy bein' green...".colorize(:green)
+
+String.color_samples
+
+puts "R".colorize(:red)
+puts "O".colorize(:orange)
+puts "Y".colorize(:yellow)
+puts "G".colorize(:green)
+puts "B".colorize(:blue)
+puts "P".colorize(:purple)
